9f34ccfe098ba23289d101819ddda9c9e9b9a426
DanieleMaffi <DanieleMaffi@outlook.it>
2023-06-20 16:09:23 +0200

    Addded backend handling of file uplading(HEX)

--- controllers/form.js
+++ controllers/form.js
@@ -1,4 +1,5 @@
 const sql = require("mssql");
+const multer = require('multer');
 const { promisify } = require("util");
 const path = require("path")
 const ejs = require('ejs')
@@ -67,6 +68,10 @@ exports.sendForm = async (req, res) => {
         //fetches the varibales from the form submission
         let posizione = req.body.posizione
         let descrizione = req.body.descrizione
+        let fileType = req.file.mimetype
+        let fileName = req.file.originalname
+        let encoding = req.file.encoding
+        let size = req.file.size
         let decodedToken = await promisify(jwt.verify)(req.cookies['token'], process.env.JWT_SECRET)
 
         await sql.connect(config)
@@ -79,5 +84,28 @@ exports.sendForm = async (req, res) => {
             console.log(err)
             res.render('response', { id: results.recordset[0].ID })   //renders response page and outputs id of the database row that has just been input
         })
+        try {
+
+            let blobData = req.file.buffer;
+
+            // Create a SQL Server connection pool
+            const pool = await sql.connect(config);
+
+            console.log(req.file)
+
+            // Define the SQL query to insert the BLOB data
+            query = 'INSERT INTO my_table (data, nome, tipo, encoding, peso) VALUES (@blobData, @fileName, @fileType, @encoding, @peso)';
+            request = pool.request()
+            request.input('blobData', sql.VarBinary, blobData)
+            request.input('fileName', sql.NChar, fileName)
+            request.input('fileType', sql.NChar, fileType)
+            request.input('encoding', sql.NChar, encoding)
+            request.input('peso', sql.Int, size)
+
+            // Execute the SQL query
+            await request.query(query);
+        } catch (err) {
+            console.log(err)
+        }
     } catch (err) { console.log(err) }
 }
--- routes/upload.js
+++ routes/upload.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const multer = require('multer');
 const formController = require("../controllers/form.js");
 const authController = require("../controllers/auth.js");
+const storage = multer.memoryStorage(); // Store files in memory
+const upload = multer({ storage });
 
 const router = express.Router();
 
-router.post('/sendForm', authController.isLoggedIn, formController.sendForm);  //When a post request is made to /upload/sendForm the formController.sendForm function is called.
+router.post('/sendForm', authController.isLoggedIn, upload.single('allegato'), formController.sendForm);  //When a post request is made to /upload/sendForm the formController.sendForm function is called.
 
 module.exports = router;
